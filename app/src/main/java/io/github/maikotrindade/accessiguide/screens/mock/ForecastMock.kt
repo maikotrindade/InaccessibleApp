fa6dc3b4e81f456e89bc4321c7db109997bbb12b
maikotrindade <maikocharles@gmail.com>
2024-11-27 15:56:47 -0700

    added ForecastScreen

---
+++ app/src/main/java/io/github/maikotrindade/accessiguide/screens/mock/ForecastMock.kt
@@ -0,0 +1,29 @@
+package io.github.maikotrindade.accessiguide.screens.mock
+
+val citiesWeather = listOf(
+    "Calgary" to "2°C",
+    "Toronto" to "5°C",
+    "Vancouver" to "10°C",
+    "Montreal" to "3°C",
+    "Edmonton" to "1°C",
+    "Halifax" to "4°C",
+    "Winnipeg" to "-2°C",
+    "Ottawa" to "2°C",
+    "Regina" to "-5°C",
+    "Saskatoon" to "-7°C",
+    "St. John's" to "0°C",
+    "Victoria" to "8°C",
+    "Hamilton" to "4°C",
+    "Kitchener" to "3°C",
+    "London" to "3°C",
+    "Windsor" to "4°C",
+    "Mississauga" to "4°C",
+    "Brampton" to "3°C",
+    "Vaughan" to "3°C",
+    "Markham" to "3°C",
+    "Richmond Hill" to "3°C",
+    "Burnaby" to "6°C",
+    "Surrey" to "6°C",
+    "Abbotsford" to "5°C",
+    "Richmond" to "6°C"
+)
