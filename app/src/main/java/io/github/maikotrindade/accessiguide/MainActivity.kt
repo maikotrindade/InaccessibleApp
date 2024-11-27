28facd7f6ab5a1be4dd5f07f4d709623d8a95181
maikotrindade <maikocharles@gmail.com>
2024-11-27 15:57:00 -0700

    updated main activity

--- app/src/main/java/io/github/maikotrindade/accessiguide/MainActivity.kt
+++ app/src/main/java/io/github/maikotrindade/accessiguide/MainActivity.kt
@@ -11,6 +11,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import io.github.maikotrindade.accessiguide.screens.ForecastScreen
+import io.github.maikotrindade.accessiguide.screens.LoginScreen
 import io.github.maikotrindade.accessiguide.ui.theme.AccessiguideTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +23,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AccessiguideTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            App()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AccessiguideTheme {
-        Greeting("Android")
+fun App() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") { LoginScreen(navController) }
+        composable("cities") { ForecastScreen() }
     }
 }
